be64ac67e2d2997711cea989eaed7cacb92dd8ab
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-06-10 01:54:43 +0300

    Homework

--- Seminar_2/src/main/java/org/example/Main.java
+++ Seminar_2/src/main/java/org/example/Main.java
@@ -19,10 +19,10 @@ public class Main {
     }
 
     public static String reverse(String str) {
-        String str1 = "";
+        StringBuilder str1 = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; --i) {
-            str1 += str.charAt(i);
+            str1.append(str.charAt(i));
         }
-        return str1;
+        return str1.toString();
     }
 }
