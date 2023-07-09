a3c8cc4573cc7b635206213732b98066a0aefcf1
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-07-09 07:53:51 +0300

    Homework 09/07/23

--- Seminar_9/src/main/java/org/example/Container.java
+++ Seminar_9/src/main/java/org/example/Container.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class Container {
+public class Container implements Comparable<Container> {
     private ArrayList<Box> arrayList = new ArrayList<>();
     private int boxCounter;
 
@@ -24,5 +24,26 @@ public class Container {
         for (Box box : arrayList){
             System.out.println("Размер " + box.weight + " кг.");
         }
+    }
+
+    public int getWeight(){
+        int containerWeight = 0;
+        for (Box box : arrayList){
+            containerWeight += box.getWeight();
+        }
+        System.out.println("Общий вес контейнера " + containerWeight + " кг.");
+        return containerWeight;
+    }
+
+
+    @Override
+    public int compareTo(Container container) {
+        if(getWeight() > container.getWeight()){
+            return 1;
+        }
+        if(getWeight() < container.getWeight()){
+            return -1;
+        }
+        return 0;
     }
 }
--- Seminar_9/src/main/java/org/example/Main.java
+++ Seminar_9/src/main/java/org/example/Main.java
@@ -1,14 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Box box1 = new Box(10);
         Box box2 = new Box(12);
         Box box3 = new Box(7);
         Container container = new Container();
+        Container container2 = new Container();
+
         container.addBox(box1);
         container.addBox(box2);
         container.addBox(box3);
+        container2.addBox(box1);
+        container2.addBox(box2);
         container.getInfo();
+        container.getWeight();
+
+        ArrayList<Container> arrayList = new ArrayList<>();
+        arrayList.add(container);
+        arrayList.add(container2);
+
+        System.out.println(" ============================================== ");
+
+        for (Container containerX : arrayList){
+            containerX.getWeight();
+        }
     }
 }
