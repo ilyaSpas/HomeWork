2fc3bb7e96ca9423c0a8cb7196498474005ddfe4
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-09-13 22:47:06 +0300

    Homework at 13/09/23

--- Seminar_JavaCore_3/src/main/java/org/example/Employee.java
+++ Seminar_JavaCore_3/src/main/java/org/example/Employee.java
@@ -1,32 +1,66 @@
 package org.example;
 
-public class Employee {
+import java.util.Random;
+
+public abstract class Employee implements Comparable<Employee> {
+
+    //region Static fields
+    protected static String[] names = new String[]{"Анатолий", "Глеб", "Клим", "Мартин", "Лазарь", "Владлен", "Клим", "Панкратий", "Рубен", "Герман"};
+    protected static String[] surNames = new String[]{"Григорьев", "Фокин", "Шестаков", "Хохлов", "Шубин", "Бирюков", "Копылов", "Горбунов", "Лыткин", "Соколов"};
+    protected static Random random = new Random();
+    public static int counter = 1000;
+    private int id;
+    //endregion
+
+    //region Fields
     private String name;
     private String surName;
-    private double salary;
-    public double calculateSalary(){
-        return salary;
+    protected double salary;
+    //endregion
+
+
+    // инициализатор
+    {
+        id = ++counter;
     }
-    public Employee(String name, String surName, double salary){
+    //region Constructors
+    protected Employee() {
+        this("#Name#", "#SurName#");
+    }
+
+    protected Employee(String name, String surName) {
+        this("#Name#", "#SurName#", 500);
+    }
+
+    protected Employee(String name, String surName, double salary) {
+        if (salary < 500) {
+            throw new RuntimeException("Ставка не должна быть менее 500 рублей в час.");
+        }
         this.name = name;
         this.surName = surName;
         this.salary = salary;
+    }
+    //endregion
+
+//    public static Employee getInstance(){
+//        return new Employee(names[random.nextInt(names.length)],
+//                surNames[random.nextInt(surNames.length)],
+//                random.nextInt(30000, 250000));
+//    }
+
+    public abstract double calculateSalary();
+
+    //region Getters and Setters
+    public int getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSurName() {
         return surName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
     }
 
     public double getSalary() {
@@ -34,6 +68,24 @@ public class Employee {
     }
 
     public void setSalary(double salary) {
+        if (salary < 30_000) {
+            throw new RuntimeException("Уровень ЗП не должен быть менее 30.000 рублей");
+        }
         this.salary = salary;
     }
+    //endregion
+
+    //region toSring
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    //endregion
 }
--- Seminar_JavaCore_3/src/main/java/org/example/Main.java
+++ Seminar_JavaCore_3/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Employee> list = Worker.getEmployees(10);
+        for(Employee employee : list){
+            System.out.println(employee);
+        }
 
+        Collections.sort(list, new EmployeeNameComparator());
+
+        System.out.println("----------------------------------");
+
+        for(Employee employee : list){
+            System.out.println(employee);
+        }
     }
 }
