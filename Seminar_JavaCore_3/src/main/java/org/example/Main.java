9f8d08e13a74a4a1091f42fab17d482555332d2b
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-09-13 22:53:34 +0300

    Homework at 13/09/23

--- Seminar_JavaCore_3/src/main/java/org/example/Main.java
+++ Seminar_JavaCore_3/src/main/java/org/example/Main.java
@@ -7,17 +7,34 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> list = Worker.getEmployees(10);
-        for(Employee employee : list){
+//        List<Employee> list = Worker.getEmployees(10);
+//        for(Employee employee : list){
+//            System.out.println(employee);
+//        }
+//
+//        Collections.sort(list, new EmployeeNameComparator());
+//
+//        System.out.println("----------------------------------");
+//
+//        for(Employee employee : list){
+//            System.out.println(employee);
+//        }
+
+        List<Employee> list1 = Worker.getEmployees(3);
+        List<Employee> list2 = Freelancer.getFreelancers(3);
+        List<Employee> list3 = foo(list1,list2);
+
+        Collections.sort(list3, new EmployeeNameComparator());
+
+        for (Employee employee : list3) {
             System.out.println(employee);
         }
 
-        Collections.sort(list, new EmployeeNameComparator());
-
-        System.out.println("----------------------------------");
-
-        for(Employee employee : list){
-            System.out.println(employee);
-        }
+    }
+    public static List<Employee> foo(List<Employee> list1, List<Employee> list2){
+        List<Employee> list3 = new ArrayList<>();
+        list3.addAll(list1);
+        list3.addAll(list2);
+        return list3;
     }
 }
