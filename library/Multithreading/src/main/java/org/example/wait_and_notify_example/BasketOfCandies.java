1a4e1858339871d7c9f8d96c1b9862be5a3fae5a
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2023-11-10 16:01:24 +0300

    New commit

--- library/Multithreading/src/main/java/org/example/wait_and_notify_example/BasketOfCandies.java
+++ library/Multithreading/src/main/java/org/example/wait_and_notify_example/BasketOfCandies.java
@@ -8,8 +8,8 @@ public class BasketOfCandies {
         amountOfCandies = 10;
     }
 
-    public synchronized void takeCandie(Child child){
-        if (amountOfCandies <= 0){
+    public synchronized void takeCandie(Child child) {
+        if (amountOfCandies <= 0) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -23,21 +23,19 @@ public class BasketOfCandies {
             System.out.println(child.getChildName() + " сьел " + child.getEatenCandies() + " конфет.");
             notify();
         }
-
     }
 
     public synchronized void putCandie(Mom mom) {
-        if (amountOfCandies == maxSizeOfBasket){
+        if (amountOfCandies == maxSizeOfBasket) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        } else {
-            amountOfCandies++;
-            System.out.println(mom.getMomName() + " положила конфету.");
-            System.out.println("В корзине осталось " + amountOfCandies + " конфет.");
-            notify();
         }
+        amountOfCandies++;
+        System.out.println(mom.getMomName() + " положила конфету.");
+        System.out.println("В корзине осталось " + amountOfCandies + " конфет.");
+        notify();
     }
 }
