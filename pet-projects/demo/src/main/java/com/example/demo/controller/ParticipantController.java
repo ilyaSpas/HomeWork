fb399c61519c536376abe72691776ee29f7820c9
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-01-15 16:21:19 +0300

    New commit

--- pet-projects/demo/src/main/java/com/example/demo/controller/ParticipantController.java
+++ pet-projects/demo/src/main/java/com/example/demo/controller/ParticipantController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Participant;
+import com.example.demo.model.Position;
 import com.example.demo.service.EventService;
 import com.example.demo.service.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/event")
@@ -21,6 +24,7 @@ public class ParticipantController {
     public String registrationForm(@ModelAttribute("participant") Participant participant,
                                    @PathVariable("id") Long id, Model model) {
         model.addAttribute("participants", eventService.findById(id).get().getParticipants());
+        model.addAttribute("positions", Arrays.asList(Position.values()));
         return "person/registrationPage";
     }
 
