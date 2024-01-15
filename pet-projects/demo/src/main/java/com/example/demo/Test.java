50311661c04f2d732df37fd7235c08ef653ab15b
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-01-15 15:59:01 +0300

    New commit

---
+++ pet-projects/demo/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.example.demo.model.Position;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
--- pet-projects/demo/src/main/java/com/example/demo/model/Participant.java
+++ pet-projects/demo/src/main/java/com/example/demo/model/Participant.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -20,7 +23,39 @@ public class Participant {
     @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "pharmacy")
+    private String pharmacy;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "pharmacy_address")
+    private String pharmacyAddress;
+
     @ManyToOne(cascade = CascadeType.REFRESH)
     @JoinColumn(name = "event_id", referencedColumnName = "id")
     private Event event;
+
+    @Column(name = "time_of_registration")
+    private LocalDateTime timeOfRegistration;
+
+    @PrePersist
+    private void init(){
+        timeOfRegistration = LocalDateTime.now();
+    }
 }
