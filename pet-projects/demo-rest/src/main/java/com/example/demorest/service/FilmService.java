0fa0ccd2d3abc80a07f921e8680951973eaefc18
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-01-27 15:15:39 +0300

    New commit

--- pet-projects/demo-rest/src/main/java/com/example/demorest/service/FilmService.java
+++ pet-projects/demo-rest/src/main/java/com/example/demorest/service/FilmService.java
@@ -3,6 +3,7 @@ package com.example.demorest.service;
 import com.example.demorest.dto.FilmDto;
 import com.example.demorest.entity.Film;
 import com.example.demorest.repo.FilmRepository;
+import com.example.demorest.util.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,13 @@ import java.util.stream.Collectors;
 @Service
 public class FilmService {
     private final FilmRepository filmRepository;
+    private final Converter converter;
 
 
     @Autowired
-    public FilmService(FilmRepository filmRepository) {
+    public FilmService(FilmRepository filmRepository, Converter converter) {
         this.filmRepository = filmRepository;
+        this.converter = converter;
     }
 
     public Film save(Film film) {
