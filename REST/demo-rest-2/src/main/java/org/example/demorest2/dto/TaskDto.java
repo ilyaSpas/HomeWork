329dd0ca049946ec810120c134a3203dac2f9ec8
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-02-05 19:39:06 +0300

    New commit

--- REST/demo-rest-2/src/main/java/org/example/demorest2/dto/TaskDto.java
+++ REST/demo-rest-2/src/main/java/org/example/demorest2/dto/TaskDto.java
@@ -1,17 +1,22 @@
 package org.example.demorest2.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Null;
 import lombok.Data;
 
 @Data
+@Schema(description = "Сущность задачи")
 public class TaskDto {
+    @Schema(description = "Название")
     @NotEmpty(message = "Title should not be empty!")
     private String title;
 
+    @Schema(description = "Описание")
     @NotEmpty(message = "Description should not be empty!")
     private String description;
 
+    @Schema(description = "Статус")
     @Null(message = "Status set automatically!")
     private String status;
 }
